67cdd6ff8009d314b8cdf2c25d3a3ad7026a7a9b
VixNepenthes <viht61220@gmail.com>
2024-07-23 14:50:34 +0700

    install axios and update some api

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -3,16 +3,26 @@ import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
 import { mockData } from '~/apis/mock-data'
+import { useEffect, useState } from 'react'
+import { fetchBoardDetailsAPI } from '~/apis'
 
 function Board() {
+  const [board, setBoard] = useState(null)
+
+  useEffect(() => {
+    // Tạm thời fix cứng boardId, flow chuẩn sử dụng react-router-dom để lấy boardId từ URL về
+    const boardId = '669f4e87cf8bbd65736517c2'
+    // Call Api
+    fetchBoardDetailsAPI(boardId).then((board) => {
+      setBoard(board)
+    })
+  }, [])
+
   return (
-    <Container
-      disableGutters
-      maxWidth={false}
-      sx={{ height: '100vh' }}>
+    <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar />
-      <BoardBar board={mockData?.board} />
-      <BoardContent board={mockData?.board} />
+      <BoardBar board={board} />
+      <BoardContent board={board} />
     </Container>
   )
 }
