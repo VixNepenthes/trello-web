43cc87011a248629f4c22c9a912add16790e0ac5
VixNepenthes <viht61220@gmail.com>
2024-07-28 14:32:02 +0700

    fix BUILD_MODE dev and production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,9 @@
-// Some constants
-// export const API_ROOT = 'http://localhost:8017'
-export const API_ROOT = 'https://trello-api-hxxt.onrender.com'
+let apiRoot = ''
+// import.meta.env
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-api-hxxt.onrender.com'
+}
+export const API_ROOT = apiRoot
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Cho phép sử dụng được process.env, mặc định phải sử dụng import.meta.env
+  define: {
+    'process.env': process.env
+  },
   plugins: [react(), svgr()],
   // base: './'
   resolve: {
