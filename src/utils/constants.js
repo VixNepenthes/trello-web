a9ef2fc32ca4a9cb68055fe74569230e4a720ad1
VixNepenthes <viht61220@gmail.com>
2024-07-28 13:59:52 +0700

    test deploy node engine 18.x

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
 // Some constants
-export const API_ROOT = 'http://localhost:8017'
+// export const API_ROOT = 'http://localhost:8017'
+export const API_ROOT = 'https://trello-api-hxxt.onrender.com'
