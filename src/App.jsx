05c93822e0058fe9797af17339d58afc5b3dac6f
Vĩ <viht61220@gmail.com>
2024-10-28 02:17:23 +0700

    FEAT | Update Login, Register Page, apply react-hook-form

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 
 import Board from './pages/Boards/_id'
 import NotFound from './pages/404/NotFound'
+import Auth from './pages/Auth/Auth'
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/boards/66a082dd33de8a5b604d8c53" replace={true} />} />
       <Route path="/boards/:boardId" element={<Board />} />
+
+      <Route path="/login" element={<Auth />} />
+      <Route path="/register" element={<Auth />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   )
